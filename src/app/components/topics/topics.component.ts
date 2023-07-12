542f773eda6c4a4f8c7afa28c923209f52f062e4
Jurrit van der Ploeg <jurritvdploeg@hotmail.com>
2023-07-12 17:15:53 +0200

    fix(topics-nested-subscribe): use switch map

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -4,12 +4,14 @@ import {
   Component,
   OnDestroy,
   OnInit,
-  inject
+  computed,
+  inject,
+  signal
 } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { Subject, filter, map, takeUntil } from 'rxjs';
+import { Subject, filter, map, switchMap, take, takeUntil } from 'rxjs';
 
-import { RouteDiscover, Topic } from '../../models';
+import { ArchiveTopics, RouteDiscover, Topic } from '../../models';
 import { ArchiveService } from '../../services';
 import { SharedModule } from '../../shared';
 
@@ -28,19 +30,35 @@ export class TopicsComponent implements OnDestroy, OnInit {
   private cdr = inject(ChangeDetectorRef);
   private router = inject(Router);
 
+  private topicId = signal('');
+  private parentTopic = computed(
+    () => this.topicId()?.split('-').slice(-1)[0] || ''
+  );
+
   private archiveId!: string;
 
   private unsubscribe$ = new Subject<void>();
 
   public ngOnInit(): void {
     this.getTopics(this.router.url);
+    const getArchive$ = this.archiveService.getArchive(this.archiveId);
+
+    getArchive$.pipe(
+      take(1)
+    ).subscribe(archiveData => {
+      this.setTopics(archiveData);
+    });
 
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(event => (event as NavigationEnd).url),
+      switchMap(url => {
+        this.getTopics(url);
+        return getArchive$;
+      }),
       takeUntil(this.unsubscribe$)
-    ).subscribe(url => {
-      this.getTopics(url);
+    ).subscribe(archiveData => {
+      this.setTopics(archiveData);
     });
   }
 
@@ -49,31 +67,25 @@ export class TopicsComponent implements OnDestroy, OnInit {
     return `assets/mock/images/${this.archiveId}/${parsedId}.svg`;
   }
 
+  public setParentLabel(parent: string): string {
+    return parent.split('-').slice(-1)[0];
+  }
+
   private getTopics(rawUrl: string): void {
     const url = rawUrl.slice(1).split('/');
     this.archiveId = url[RouteDiscover.Archive];
-    const topicId = url[RouteDiscover.Topic];
-    const parentTopic = topicId?.split('-').slice(-1)[0] || '';
+    this.topicId.set(url[RouteDiscover.Topic]);
+  }
 
-    // ! Fix nested subscribe
-    this.archiveService.getArchive(this.archiveId).pipe(
-      takeUntil(this.unsubscribe$)
-    ).subscribe(archiveData => {
-      this.topics = archiveData.topics.filter(topic =>
-        // Define childs of parent topic
-        topic.id.startsWith(topicId) &&
-        // Only take direct children by validating length opposed to parent
-        topic.id.length > topicId.length &&
-        topic.id.length <= topicId.length + parentTopic.length + 3
-      ).map(topic => {
-        if (topic.parent) {
-          // The alternative parent for a topic
-          topic.parent = topic.parent.split('-').slice(-1)[0].toUpperCase();
-        }
-        return topic;
-      });
-      this.cdr.detectChanges();
-    });
+  private setTopics(archiveData: ArchiveTopics): void {
+    this.topics = archiveData.topics.filter(topic =>
+      // Define childs of parent topic
+      topic.id.startsWith(this.topicId()) &&
+      // Only take direct children by validating length opposed to parent
+      topic.id.length > this.topicId().length &&
+      topic.id.length <= this.topicId().length + this.parentTopic().length + 3
+    );
+    this.cdr.detectChanges();
   }
 
   public ngOnDestroy(): void {
